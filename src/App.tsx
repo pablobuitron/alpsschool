9c37d8692928514aff648b25638b1cf7b2e520e7
Pablo Buitrón <pablobuitron@maxwell.local>
2025-11-24 14:12:08 +0100

    small changes added

--- src/App.tsx
+++ src/App.tsx
@@ -270,7 +270,7 @@ const AboutSection: React.FC = () => (
         <strong>The maximum number of participants will be limited to 50.</strong>
       </p>
 
-      <div className="flex justify-center items-center mt-[12vh] mb-[4vh]">
+      <div className="flex justify-center items-center mt-32 mb-6">
         <img
           src="./sponsor.jpeg"
           alt="Sponsor"
